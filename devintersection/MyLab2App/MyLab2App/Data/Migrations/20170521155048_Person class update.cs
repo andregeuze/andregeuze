90fe37e7fd19a22faa086e4075474dc6a25146fb
André Geuze <andre.geuze@avanade.com>
2017-11-07 19:46:32 +0100

    add devintersection projects

---
+++ devintersection/MyLab2App/MyLab2App/Data/Migrations/20170521155048_Person class update.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyLab2App.Data.Migrations
+{
+    public partial class Personclassupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ devintersection/MyNewApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MyNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello DevIntersection!");
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(context => context.Response.WriteAsync("Hello World!")))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
