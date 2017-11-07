87f0a661a4d7ebdd14af0db0a6bc515e25b1d38b
André Geuze <andre.geuze@avanade.com>
2017-11-07 19:49:52 +0100

    add bouwblok proof of concept

---
+++ majestic-bouwblok/MajesticBouwblok/ClientApp/components/MajesticForm.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { RijbewijsBlock } from './blocks/RijbewijsBlock';
+
+export class MajesticForm extends React.Component<RouteComponentProps<{}>, {}> {
+
+    public render() {
+        return <div>
+            <h1>Hello, majestic world!</h1>
+
+            <RijbewijsBlock />
+        </div>;
+    }
+
+
+}
