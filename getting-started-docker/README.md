24c20a52b786d78908cbd63817ae3e2aecc34b74
andregeuze <andregeuze@gmail.com>
2019-01-07 21:32:51 +0100

    add prototype for docker setup with live editing enabled

---
+++ getting-started-docker/README.md
@@ -0,0 +1,1 @@
+# Getting started with Docker for local development
---
+++ getting-started-docker/src/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+
+<?php
+
+echo $_SERVER["DOCUMENT_ROOT"];
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
+
+</body>
+</html>
